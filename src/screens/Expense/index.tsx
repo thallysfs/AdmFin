cd701de35d2c410fd24210e1f89abdb2925d0483
thallysfs <thallys@hotmail.com>
2022-03-23 09:34:12 -0300

    ajustes svg

--- src/screens/Expense/index.tsx
+++ src/screens/Expense/index.tsx
@@ -1,10 +1,73 @@
 import React from 'react'
-import { Container } from './styles'
+import { Keyboard, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Image } from 'react-native'
+import { HeaderBar } from '../../components/HeaderBar'
+import RNPickerSelect from 'react-native-picker-select';
+
+import { 
+  Container, 
+  Fields, 
+  Form, 
+  Header,
+  Title,
+  SelectItem 
+} from './styles'
+import SendMoneySvg from '../../assets/sendMoney.svg'
+import { InputForm } from '../../components/Form/InputForm'
+import { useForm } from 'react-hook-form'
+import { Button } from '../../components/Form/Button'
+import { ScrollView } from 'react-native-gesture-handler'
 
 export function Expense(){
+
+  const {
+    control,
+    handleSubmit
+  } = useForm();
+
+  function handleRegister(form: any){
+    console.log(form)
+  }
+
   return(
-    <Container>
-      
-    </Container>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView behavior='position'>
+      <ScrollView>         
+        <Container>
+          <HeaderBar />
+          <Header>
+            <Title>Despesas</Title>
+            <SendMoneySvg width="320" height="160" />
+          </Header>
+            <Form>
+              <Fields>
+                <InputForm
+                  name="descrition" 
+                  control={control}
+                  placeholder="Descrição"
+                  iconName='align-left'
+                />
+                <InputForm
+                  name="value" 
+                  control={control}
+                  placeholder="Valor"
+                  iconName='dollar-sign'
+                />
+                <InputForm
+                  name="obs" 
+                  control={control}
+                  placeholder="Observação"
+                  iconName='align-left'
+                />
+
+              </Fields>
+              <Button 
+                title='Salvar'
+                onPress={handleSubmit(handleRegister)}
+              />
+            </Form>
+          </Container>
+        </ScrollView>   
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   )
 }
--- src/screens/Expense/styles.ts
+++ src/screens/Expense/styles.ts
@@ -1,6 +1,44 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native'
 
 
 export const Container = styled.View`
 
 `;
+
+export const Header = styled.View`
+    width: 100%;
+    align-items: center
+    height: ${RFValue(65)}px;
+`;
+
+export const Title = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.title};
+    font-size: ${RFValue(30)}px;
+    padding: 15px 25px;
+
+`
+// export const Content = styled.View`
+//     padding: 0 24px;
+// `;
+
+export const Form = styled.View`
+    justify-content: space-between;
+    width: 100%;
+    margin-top: 190px ;
+    padding: 0 24px;
+
+`;
+
+export const Fields = styled.View`
+
+`;
+
+export const SelectItem = styled.View`
+    flex: 1;
+    backgroundColor: ${({ theme }) => theme.colors.background_secondary};
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 10px;
+`;
+
